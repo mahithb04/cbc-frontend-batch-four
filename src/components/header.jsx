db8d85b6386ab2f48dae1c6b76d233d8212b7ea4
Mahith <mahithroxx@gmail.com>
2025-04-25 20:43:16 +0530

    Day 12🧨

---
+++ src/components/header.jsx
@@ -0,0 +1,9 @@
+
+export default function Header(){
+    return(
+        <header className = "w-full bg-blue-500 h-[50px] text-white text-[20px]">
+            <h1>My Store</h1>
+            
+        </header>
+    )
+}
