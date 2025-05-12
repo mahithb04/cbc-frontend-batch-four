90272188b772a0615215d9ed9f5a47e7f9b35ea3
Mahith <mahithroxx@gmail.com>
2025-05-12 12:07:43 +0530

    Day 17🧨

--- src/pages/client/productOverview.jsx
+++ src/pages/client/productOverview.jsx
@@ -3,6 +3,8 @@ import { useEffect, useState } from "react"
 import toast from "react-hot-toast"
 import { useParams } from "react-router-dom"
 import Loader from "../../components/loader"
+import ImageSlider from "../../components/imageSlider"
+import getCart, { addToCart } from "../../utils/cart"
 
 export default function ProductOverview(){
 
@@ -21,7 +23,7 @@ export default function ProductOverview(){
                 axios.get(import.meta.env.VITE_BACKEND_URL+"/api/product/"+params.id).then(
                     (res)=>{
                         console.log(res)
-                        setProduct(res.data)
+                        setProduct(res.data.product)
                         setStatus("loaded")
                     }
                 ).catch(
@@ -39,8 +41,44 @@ export default function ProductOverview(){
                 status == "loading"&&<Loader/>
             }
             {
-                status == "loaded"&& <div className="w-full h-full">
-                    Product Loaded
+                status == "loaded"&&
+                <div className="w-full h-full flex">
+                    <div className="w-[50%] h-full">
+                        <ImageSlider images={product.images}/>
+                    </div>
+                    <div className="w-[50%] h-full p-[40px]">
+                        <h1 className="text-3xl font-bold text-center mb-[40px]">{product.name}{" | "}<span className="text-2xl font-semibold text-center text-gray-500">{product.altNames.join(" | ")}</span></h1>
+                        <div className="w-full flex justify-center mb-[40px]">
+                            {
+                                product.labeledPrice>product.price?(
+                                    <>
+                                        <h2 className="text-2xl mr-[20px]">LKR: {product.price.toFixed(2)}</h2>
+                                        <h2 className="text-2xl line-through text-gray-500">LKR: {product.labeledPrice.toFixed(2)}</h2>
+                                
+                                    </>
+                                ) : (
+                                    <h2>{product.price}</h2>
+                                )
+                                    
+                            }
+                        </div>
+                        <p className="text-xl text-center text-gray-500 mb-[40px] border">{product.description}</p>
+                        <div className="w-full flex justify-center mb-[40px]">
+                            <button className="bg-pink-800 border border-pink-800 cursor-pointer text-white w-[200px] h-[50px] rounded-lg hover:bg-white hover:text-pink-800 transition-all duration-300 ease-in-out mr-[20px]" onClick={
+                                ()=>{
+                                    addToCart(product, 1)
+                                    toast.success("Product added to cart")
+                                    console.log(getCart())
+                                }
+                            }>
+                                Add to Cart
+                            </button>
+                            <button className="bg-pink-800 border border-pink-800 cursor-pointer text-white w-[200px] h-[50px] rounded-lg hover:bg-white hover:text-pink-800 transition-all duration-300 ease-in-out">
+                                Buy Now
+                            </button>
+                        </div>
+
+                    </div>
                 </div>
             }
             {
--- src/pages/homePage.jsx
+++ src/pages/homePage.jsx
@@ -2,16 +2,18 @@ import { Route, Routes } from "react-router-dom";
 import Header from "../components/header";
 import ProductsPage from "./client/productsPage";
 import ProductOverview from "./client/productOverview";
+import CartPage from "./client/cart";
 
 export default function HomePage(){
     return(
         <div className="w-full h-screen">
             <Header/>
-            <div className="w-full min-h-[calc(100vh-50px)]">
+            <div className="w-full h-[calc(100vh-50px)] min-h-[calc(100vh-50px)]">
                 <Routes path = "/*">
                     <Route path="/" element={<h1>Home Page</h1>}/>
                     <Route path="/products" element={<ProductsPage/>}/>
                     <Route path="/overview/:id" element={<ProductOverview/>}/>
+                    <Route path="/cart" element={<CartPage/>}/>
                     <Route path="/*" element={<div>404 Not Found</div>}/>
                 </Routes>
             </div>
