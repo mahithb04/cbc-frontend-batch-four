a82f21f551ea46b43bf5d513a57653eb1faee744
Mahith <mahithroxx@gmail.com>
2025-05-17 12:11:50 +0530

    Day 18🧨

--- src/utils/cart.js
+++ src/utils/cart.js
@@ -45,3 +45,21 @@ export function removeFromCart(productId){
     localStorage.setItem("cart", JSON.stringify(cart))
     return cart
 }
+
+export function getTotal(){
+    let cart = getCart();
+    let total = 0;
+    cart.forEach((product) => {
+        total += product.price * product.quantity
+    })
+    return total
+}
+
+export function getTotalForLabeledPrice(){
+    let cart = getCart();
+    let total = 0;
+    cart.forEach((product) => {
+        total += product.labeledPrice * product.quantity
+    })
+    return total
+}
